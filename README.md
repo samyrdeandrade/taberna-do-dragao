5169d9d8ec1aaef19ff0e5cf0227863912a4e060
samyrdeandrade <jogadorsamyr@gmail.com>
2025-04-01 22:04:05 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taberna do Dragão
+
+Projeto de RPG com 3D&T, ficha online e IA como Mestre.
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo à Taberna do Dragão!</h1>
+      <p>Em breve, a aventura começa...</p>
+    </div>
+  )
+}
